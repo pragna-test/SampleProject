fefd2060406143844405f2ecfff5d1e03a4f1972
Pragna <pragna.varun123@gmail.com>
2020-10-22 15:52:49 +1100

    this is testing of local

---
+++ TestForGit/src/myTest/sampleTest.java
@@ -0,0 +1,12 @@
+package myTest;
+
+public class sampleTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("this is printing");
+
+	}
+
+}
